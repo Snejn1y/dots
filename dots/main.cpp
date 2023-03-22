a1bf1ad9130402a0a34cf0d99bfd4c5a157ff43d
Денис Луцак <cheburatovich@gmail.com>
2023-03-22 20:36:37 +0200

    Зробив повтор в коді

--- dots/main.cpp
+++ dots/main.cpp
@@ -18,10 +18,12 @@ protected:
     double radius;  // радіус кулі
 public:
     Sphere(double x, double y, double r) : Point(x, y), radius(r) {}
-    double area() {  // площа бічної поверхні кулі
+    double area() // площа бічної поверхні кулі
+    {  
         return 4 * M_PI * radius * radius;
     }
-    double volume() {  // об'єм кулі
+    double volume() // об'єм кулі
+    {  
         return 4.0 / 3.0 * M_PI * radius * radius * radius;
     }
 };
@@ -40,28 +42,43 @@ protected:
     double radius;  // радіус кульового сегмента
 public:
     SphericalSegment(double x, double y, double l, double r) : Segment(x, y, l), radius(r) {}
-    double area() { // площа кульового сегмента
+    double area() // площа кульового сегмента
+    { 
         return 2 * M_PI * radius * length;
     }
-    double volume() { // об'єм кульового сегмента
+    double volume() // об'єм кульового сегмента
+    { 
         return M_PI * length * length * (radius - length / 3);
     }
 };
 
 int main() {
-    double x, y, r, l, seg_r;
-    cout << "Enter coordinates and radius for the sphere: ";
-    cin >> x >> y >> r;
-    Sphere sphere(x, y, r);
-    cout << "Area of the sphere: " << sphere.area() << endl;
-    cout << "Volume of the sphere: " << sphere.volume() << endl;
-    cout << "Enter coordinates and length for the segment: ";
-    cin >> x >> y >> l;
-    Segment segment(x, y, l);
-    cout << "Enter radius for the spherical segment: ";
-    cin >> seg_r;
-    SphericalSegment sphericalSegment(x, y, l, seg_r);
-    cout << "Area of the spherical segment: " << sphericalSegment.area() << endl;
-    cout << "Volume of the spherical segment: " << sphericalSegment.volume() << endl;
+    while (true)
+    {
+        double x, y, r, l, seg_r;
+        cout << "Enter the coordinates of the sphere, first the x-coordinate and then the y-coordinate: ";
+        cin >> x >> y;
+        cout << "Enter the radius of the sphere: ";
+        cin >> r;
+        Sphere sphere(x, y, r);
+        cout << "Area of the sphere: " << sphere.area() << endl;
+        cout << "Volume of the sphere: " << sphere.volume() << endl;
+        cout << "Enter the coordinates for the segment, first the x-coordinate and then the y-coordinate: ";
+        cin >> x >> y;
+        cout << "Enter the height of the segment: ";
+        cin >> l;
+        Segment segment(x, y, l);
+        cout << "Enter radius for the spherical segment: ";
+        cin >> seg_r;
+        SphericalSegment sphericalSegment(x, y, l, seg_r);
+        cout << "Area of the spherical segment: " << sphericalSegment.area() << endl;
+        cout << "Volume of the spherical segment: " << sphericalSegment.volume() << endl;
+
+        bool a;
+        cout << "Would you like to do it again?\n"
+            ">>>";
+        cin >> a;
+        if (!a) break;
+    }
     return 0;
 }
